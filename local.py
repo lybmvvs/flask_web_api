3237ec6f96c4882446b1a9230fc45e50cb36f384
lybmvvs <stik9543@gmail.com>
2022-05-25 18:45:53 +0300

    get and delete are completed now, post takes some time

--- local.py
+++ local.py
@@ -1,4 +1,7 @@
 import requests
 
-result = requests.get("http://127.0.0.1:3000/api/intersection")
+
+result = requests.get("http://127.0.0.1:3000/api/vlpandipr/data")
+result = requests.delete("http://127.0.0.1:3000/api/vlpandipr/vlp")
+result = requests.post("http://127.0.0.1:3000/api/vlpandipr/vlp", json={"q_liq": [0,10,20], "p_wf": [85,95,100]})
 print(result.json())
--- main.py
+++ main.py
@@ -1,14 +1,61 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+import numpy as np
+from scipy import interpolate
+
 
 app = Flask(__name__)
 api = Api()
 
-class Main(Resource):
-    def get(self):
-        return {"pwf": 100, "q_liq": 50}
+vlpandipr={
+    "ipr":{"q_liq": [0,10,30], "p_wf": [100,90,80]},
+    "vlp": {"q_liq": [0,10,20], "p_wf": [85,95,100]}
+}
 
-api.add_resource(Main, "/api/intersection")
+class Main(Resource):
+    def get(self,vlpandipr_status):
+        if vlpandipr_status == 'data':
+            return vlpandipr
+        if vlpandipr_status == 'result':
+            x_ipr = vlpandipr['ipr']['q_liq']
+            y_ipr = vlpandipr['ipr']['p_wf']
+            x_vlp = vlpandipr['vlp']['q_liq']
+            y_vlp = vlpandipr['vlp']['p_wf']
+            ipr = interpolate.interp1d(x_ipr, y_ipr, fill_value='extrapolate')
+            vlp = interpolate.interp1d(x_vlp, y_vlp, fill_value='extrapolate')
+            max_values = []
+            max_values.append(max(x_ipr))
+            max_values.append(max(x_vlp))
+            xnew = np.linspace(0, max(max_values), max(max_values) * 1000 + 1)
+
+            def myIntersection(fun1, fun2, x0):
+                x_coord = []
+                for i in x0:
+                    if fun1(i) == fun2(i):
+                        x_coord.append(i)
+                    else:
+                        pass
+                return x_coord
+
+            a = myIntersection(ipr, vlp, xnew)
+            b = list(ipr(a))
+            output_file = {"q_liq": a, "p_wf": b}
+            return output_file
+        else:
+            return vlpandipr[vlpandipr_status]
+
+    def delete(self,vlpandipr_status):
+        del vlpandipr[vlpandipr_status]
+        return vlpandipr
+
+    def post(self,vlpandipr_status):
+        parser = reqparse.RequestParser()
+        parser.add_argument("q_liq", type = list)
+        parser.add_argument("p_wf", type = list)
+        vlpandipr[vlpandipr_status] = parser.parse_args()
+        return vlpandipr
+
+api.add_resource(Main, "/api/vlpandipr/<string:vlpandipr_status>")
 api.init_app(app)
 
 
