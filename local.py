c10dca31007f1f3c14784d503094175edae8f0dd
lybmvvs <stik9543@gmail.com>
2022-06-07 22:24:02 +0300

    still trying swagger but not succeeded

--- local.py
+++ local.py
@@ -2,7 +2,7 @@ import requests
 
 
 result = requests.get("http://127.0.0.1:3000/api/vlpandipr/ipr")
-
+result = requests.get("http://127.0.0.1:3000/api/vlpandipr/data")
 #result = requests.put("http://127.0.0.1:3000/api/vlpandipr/ipr", json={"q_liq": [0,5,15], "p_wf": [90,80,70]})
 
 print(result.json())
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Blueprint, Flask, jsonify, url_for
 from flask_restful import Api, Resource, reqparse,fields, marshal_with
 import numpy as np
 from scipy import interpolate
+from flasgger import Swagger, swag_from
+import config
 
 
 
 
 app = Flask(__name__)
-api = Api()
+api = Api(app)
+# swagger config
+app.config['SWAGGER'] = {
+    'title': 'My API',
+    'uiversion': 3,
+    "specs_route": "/swagger/"
+}
+swagger = Swagger(app)
+app.config.from_object(config.Config)
 
 
 
@@ -19,14 +29,11 @@ vlpandipr={
 }
 
 class Main(Resource):
+    @swag_from('openapi.yaml')
     def get(self,vlpandipr_status):
-        """
 
-        :param vlpandipr_status:
-        :return:
-        """
         if vlpandipr_status == 'data':
-            return vlpandipr
+            return jsonify(vlpandipr)
         if vlpandipr_status == 'result':
             x_ipr = vlpandipr['ipr']['q_liq']
             y_ipr = vlpandipr['ipr']['p_wf']
@@ -51,31 +58,32 @@ class Main(Resource):
             a = myIntersection(ipr, vlp, xnew)
             b = list(ipr(a))
             output_file = {"q_liq": a, "p_wf": b}
-            return output_file
+            return jsonify(output_file)
         else:
-            return vlpandipr[vlpandipr_status]
+            return jsonify(vlpandipr[vlpandipr_status])
 
     def delete(self,vlpandipr_status):
         del vlpandipr[vlpandipr_status]
-        return vlpandipr
+        return jsonify(vlpandipr)
 
     def post(self,vlpandipr_status):
         parser = reqparse.RequestParser()
         parser.add_argument("q_liq", action='append', type=int)
         parser.add_argument("p_wf", action='append', type=int)
         vlpandipr[vlpandipr_status] = parser.parse_args()
-        return vlpandipr
+        return jsonify(vlpandipr)
 
     def put(self, vlpandipr_status):
         parser = reqparse.RequestParser()
         parser.add_argument("q_liq", action='append', type=int)
         parser.add_argument("p_wf", action='append', type=int)
         vlpandipr[vlpandipr_status] = parser.parse_args()
-        return vlpandipr
+        return jsonify(vlpandipr)
 
 
 api.add_resource(Main, "/api/vlpandipr/<string:vlpandipr_status>")
 api.init_app(app)
+
 
 
 if __name__ == "__main__":
